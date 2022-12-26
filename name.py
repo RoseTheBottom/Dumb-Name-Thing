6dbda29b4126f0a42b3a30dcadcca66411b36f95
RoseTheBottom <73193021+RoseTheBottom@users.noreply.github.com>
2022-12-26 21:08:30 +0000

    Add files via upload

---
+++ name.py
@@ -0,0 +1,18 @@
+import time
+
+name = input('What is your name?\n')
+lower = name.lower
+stupid = ["egg", "james", "jeff","keith","arron","Egg", "James", "Jeff","Keith","Arron"]
+doubleA = ["Aaron","Aaliyah","aaron","aaliyah"]
+if name in stupid:
+    print(name, ', thats a stupid name')
+elif name in doubleA:
+    print("Having double letters in your name doesn't make you special.\n")
+    time.sleep(3)
+    print("You're just stupid \n")
+    time.sleep(5)
+    print("Go fuck yourself.")
+    time.sleep(4.5)
+    exit
+else:
+    print('Hi, %s.' % name)
